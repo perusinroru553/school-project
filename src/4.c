a5e65a8f7caaa1eb9d730cfac2fcf0573e338d5e
perusinroru553 <perusinroru553@gmail.com>
2025-03-09 07:19:39 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int age = 20;
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    } else {
+        printf("You are not eligible to vote.\n");
+    }
+    return 0;
+}
