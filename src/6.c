2129ec3603464a78f18a3b3b72f16e9026b25d5b
perusinroru553 <perusinroru553@gmail.com>
2025-03-11 02:28:05 +0000

    Add 6.c

---
+++ src/6.c
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Random C Code</title>
+  </head>
+  <body>
+    <h1>Random C Code</h1>
+    <p>This code was generated using a random number generator.</p>
+    
+    <pre><code class="language-c">#include &lt;stdio.h&gt;
+
+int main() {
+  int x = 5;
+  printf("The value of x is: %d\n", x);
+  return 0;
+}
+</code></pre>
+    
+    <p>This code will print the value of the variable "x" to the console.</p>
+  </body>
+</html>
