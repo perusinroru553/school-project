d728c4bb35f1f0c934dbae2bae3fffabdf1833f0
perusinroru553 <perusinroru553@gmail.com>
2025-03-18 04:55:32 +0000

    Add 13.c

---
+++ src/13.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int age = 25;
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    } else {
+        printf("Sorry, you are not eligible to vote.\n");
+    }
+    return 0;
+}
