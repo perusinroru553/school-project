323fa00d0002610693d82bfcbb001a7c6a71354c
perusinroru553 <perusinroru553@gmail.com>
2025-05-27 03:32:43 +0000

    Add 49.c

---
+++ src/49.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("Positive number\n");
+    } else if (x == 0) {
+        printf("Zero\n");
+    } else {
+        printf("Negative number\n");
+    }
+    return 0;
+}
